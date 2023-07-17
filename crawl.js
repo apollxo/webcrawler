214bb6455a989280ac7e6efe9129c5b3b38836ed
umidevop <umidevop@sysXcall>
2023-07-17 14:40:10 +0500

    get urls from html

--- crawl.js
+++ crawl.js
@@ -1,3 +1,37 @@
+const { JSDOM } = require('jsdom')
+
+function getURLs(htmlBody, baseURL){
+	const urls = []
+	const dom = new JSDOM(htmlBody)
+	const linkElements = dom.window.document.querySelectorAll('a')
+	for (const linkElement of linkElements){
+		if(linkElement.href.slice(0, 1) === '/'){
+			//testing
+
+			try{
+				const urlObj = new URL(`${baseURL}${linkElement.href}`)
+				urls.push(urlObj.href)
+			}
+			catch(err){
+				console.log('Invalid URL')
+			}
+			//relative 
+		}
+		else{
+			//testing 
+			try{
+				const urlObj = new URL(linkElement.href)
+				urls.push(urlObj.href)
+			}
+			catch(err){
+				console.log('Invalid URL')
+			}
+			//absolute 
+		}
+	}
+	return urls
+}
+
 function normalizeURL(urlString) {
 	const objURL = new URL(urlString)
 	let hostPath = objURL.hostname + objURL.pathname
@@ -8,5 +42,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-	normalizeURL
+	normalizeURL,
+	getURLs
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,6 @@
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLs } = require('./crawl.js')
 const { test, expect } = require('@jest/globals')
+
 
 //Normalizing by getting only hostname and pathname
 test('normalizeURL', () => {
@@ -22,5 +23,73 @@ test('normalizeURL strip http', () => {
 	const input = 'http://blog.boot.dev/path/'
 	const actual = normalizeURL(input)
 	const expected = 'blog.boot.dev/path'
+	expect(actual).toEqual(expected)
+})
+
+
+// getting absolute url from html
+
+test('getURLs absolute urls', () => {
+	const inputHTML = `
+	<html>
+		<head></head>
+		<body>
+			<a href='https://blog.boot.dev/path'>Boot.dev</a>
+		</body>
+	</html>
+`
+	const inputBaseURL = 'https://blog.boot.dev/path'
+	const actual = getURLs(inputHTML, inputBaseURL)
+	const expected = ['https://blog.boot.dev/path']
+	expect(actual).toEqual(expected)
+})
+
+
+// getting relative urls from html
+test('getURLs relative urls', () => {
+	const inputHTML = `
+	<html>
+		<head></head>
+		<body>
+			<a href='/path'>Boot.dev</a>
+		</body>
+	</html>
+`
+	const inputBaseURL = 'https://blog.boot.dev'
+	const actual = getURLs(inputHTML, inputBaseURL)
+	const expected = ['https://blog.boot.dev/path']
+	expect(actual).toEqual(expected)
+})
+
+// getting both absolute and relative urls
+test('getURLs both urls', () => {
+	const inputHTML = `
+	<html>
+		<head></head>
+		<body>
+			<a href='/path1'>Boot.dev</a>
+			<a href='https://blog.boot.dev/path2'>Boot.dev</a>
+		</body>
+	</html>
+`
+	const inputBaseURL = 'https://blog.boot.dev'
+	const actual = getURLs(inputHTML, inputBaseURL)
+	const expected = ['https://blog.boot.dev/path1', 'https://blog.boot.dev/path2']
+	expect(actual).toEqual(expected)
+})
+
+// getting invalid urls
+test('getURLs both urls', () => {
+	const inputHTML = `
+	<html>
+		<head></head>
+		<body>
+			<a href='invalid'>Boot.dev</a>
+		</body>
+	</html>
+`
+	const inputBaseURL = 'https://blog.boot.dev'
+	const actual = getURLs(inputHTML, inputBaseURL)
+	const expected = []
 	expect(actual).toEqual(expected)
 })
